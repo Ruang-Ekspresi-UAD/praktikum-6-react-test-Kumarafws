ed972b46a7810bfa4b9ed0e2bee73f572935b9b4
Kumarafws <kumarafawwas@gmail.com>
2024-11-24 13:47:45 +0700

    Praktikum 6

--- test/counter.test.js
+++ test/counter.test.js
@@ -4,3 +4,35 @@ import '@testing-library/jest-dom';
 import Counter from './counter';
 import Display from './display';
 
+describe('Counter Component', () => {
+  test('Counter Default Value must be 0', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('count-value');
+    expect(countValue).toHaveTextContent('0');
+  });
+
+  test('Increment works when button clicked', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('count-value');
+    const incrementButton = screen.getByText('Increment');
+    fireEvent.click(incrementButton);
+    expect(countValue).toHaveTextContent('1');
+  });
+
+  test('Decrement works when button clicked', () => {
+    render(<Counter />);
+    const countValue = screen.getByTestId('count-value');
+    const decrementButton = screen.getByText('Decrement');
+    fireEvent.click(decrementButton);
+    expect(countValue).toHaveTextContent('-1');
+  });
+});
+
+describe('Display Component', () => {
+  test('Display has correct value', () => {
+    const value = 42;
+    render(<Display value={value} />);
+    const displayValue = screen.getByTestId('display-value');
+    expect(displayValue).toHaveTextContent(`Value: ${value}`);
+  });
+});
